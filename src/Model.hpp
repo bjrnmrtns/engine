8e1531977df872425c920ea7ad1c6bdceaa67a42
Bjorn Martens <bjornms@gmail.com>
2018-08-15 20:28:52 +0200

    Change to indent 4 and Column width 140 clang-format

--- src/Model.hpp
+++ src/Model.hpp
@@ -8,13 +8,9 @@ const ::Mesh::TStaticMesh Box();
 const ::Mesh::TStaticMesh Grid();
 const ::Mesh::TStaticMesh SimpleTank();
 
-const ::Mesh::TStaticMesh Scale(::Mesh::TStaticMesh mesh,
-                                const glm::vec3 factor);
-const ::Mesh::TStaticMesh Move(::Mesh::TStaticMesh mesh,
-                               const glm::vec3 translation);
+const ::Mesh::TStaticMesh Scale(::Mesh::TStaticMesh mesh, const glm::vec3 factor);
+const ::Mesh::TStaticMesh Move(::Mesh::TStaticMesh mesh, const glm::vec3 translation);
 
-const ::Mesh::TStaticMesh ChangeColor(::Mesh::TStaticMesh mesh,
-                                      glm::vec3 color);
-const ::Mesh::TStaticMesh AdjustColor(::Mesh::TStaticMesh mesh,
-                                      glm::vec3 color);
+const ::Mesh::TStaticMesh ChangeColor(::Mesh::TStaticMesh mesh, glm::vec3 color);
+const ::Mesh::TStaticMesh AdjustColor(::Mesh::TStaticMesh mesh, glm::vec3 color);
 } // namespace Model
--- src/UiElements.hpp
+++ src/UiElements.hpp
@@ -3,8 +3,5 @@
 #include "VertexDefinitions.hpp"
 #include <vector>
 
-std::vector<UI::Vertex> generateSquare(int x, int y, int w, int h,
-                                       const glm::vec3 &color);
-std::vector<UI::Vertex> SelectionRectangle(const glm::vec2 &pointA,
-                                           const glm::vec2 &pointB,
-                                           const glm::vec3 &color);
+std::vector<UI::Vertex> generateSquare(int x, int y, int w, int h, const glm::vec3 &color);
+std::vector<UI::Vertex> SelectionRectangle(const glm::vec2 &pointA, const glm::vec2 &pointB, const glm::vec3 &color);
